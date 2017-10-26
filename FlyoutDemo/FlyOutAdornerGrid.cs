3f6345808d7096e8680438abbd0389f0a2e754e8
Joshua Loschen <josh.loschen@gmail.com>
2017-10-26 13:08:37 -0500

    Got DataContext to be passed along to Flyout Content

--- FlyoutDemo/FlyOutAdornerGrid.cs
+++ FlyoutDemo/FlyOutAdornerGrid.cs
@@ -1,8 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
 
 namespace FlyoutDemo
 {
@@ -10,46 +8,32 @@ namespace FlyoutDemo
     {
         #region Dependency Properties
         public static readonly DependencyProperty IsFlyoutVisibleProperty = DependencyProperty.Register(nameof(IsFlyoutVisible), typeof(bool), typeof(FlyOutAdornerGrid), new FrameworkPropertyMetadata(IsAdornerVisible_PropertyChanged));
-        public static readonly DependencyProperty ClearFilterCommandProperty = DependencyProperty.Register(nameof(ClearFilterCommand), typeof(ICommand), typeof(FlyOutAdornerGrid), new PropertyMetadata(null));
-        public static readonly DependencyProperty ShowFilterProperty = DependencyProperty.Register(nameof(ShowFilter), typeof(bool), typeof(FlyOutAdornerGrid), new PropertyMetadata(false));
-        public static readonly DependencyProperty PlaceHolder1Property = DependencyProperty.Register(nameof(PlaceHolder1), typeof(/*object*/FrameworkElement), typeof(FlyOutAdornerGrid), new /*UI*/PropertyMetadata(null));
-        public static readonly DependencyProperty ContentXProperty = DependencyProperty.Register(nameof(ContentX), typeof(int), typeof(FlyOutAdornerGrid), new PropertyMetadata(0));
-        public static readonly DependencyProperty ContentYProperty = DependencyProperty.Register(nameof(ContentY), typeof(int), typeof(FlyOutAdornerGrid), new PropertyMetadata(0));
+        public static readonly DependencyProperty PlaceHolder1Property = DependencyProperty.Register(nameof(PlaceHolder1), typeof(FrameworkElement), typeof(FlyOutAdornerGrid), new PropertyMetadata(null));
+        public static readonly DependencyProperty MyYProperty = DependencyProperty.Register(nameof(MyY), typeof(int), typeof(FlyOutAdornerGrid), new PropertyMetadata(0));
+        public static readonly DependencyProperty MyXProperty = DependencyProperty.Register(nameof(MyX), typeof(int), typeof(FlyOutAdornerGrid), new PropertyMetadata(0));
 
-        public int ContentY
+        public int MyY
         {
-            get { return (int)GetValue(ContentYProperty); }
-            set { SetValue(ContentYProperty, value); }
+            get { return (int)GetValue(MyYProperty); }
+            set { SetValue(MyYProperty, value); }
         }
 
-        public int ContentX
+        public int MyX
         {
-            get { return (int)GetValue(ContentXProperty); }
-            set { SetValue(ContentXProperty, value); }
+            get { return (int)GetValue(MyXProperty); }
+            set { SetValue(MyXProperty, value); }
         }
 
-        public /*object*/FrameworkElement PlaceHolder1
+        public FrameworkElement PlaceHolder1
         {
-            get { return (/*object*/FrameworkElement)GetValue(PlaceHolder1Property); }
+            get { return (FrameworkElement)GetValue(PlaceHolder1Property); }
             set { SetValue(PlaceHolder1Property, value); }
-        }
-        
-        public ICommand ClearFilterCommand
-        {
-            get { return (ICommand)GetValue(ClearFilterCommandProperty); }
-            set { SetValue(ClearFilterCommandProperty, value); }
         }
 
         public bool IsFlyoutVisible
         {
             get { return (bool)GetValue(IsFlyoutVisibleProperty); }
             set { SetValue(IsFlyoutVisibleProperty, value); }
-        }
-
-        public bool ShowFilter
-        {
-            get { return (bool)GetValue(ShowFilterProperty); }
-            set { SetValue(ShowFilterProperty, value); }
         }
 
         private static void IsAdornerVisible_PropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
@@ -129,44 +113,37 @@ namespace FlyoutDemo
             {
                 HorizontalAlignment = HorizontalAlignment.Left,
                 VerticalAlignment = VerticalAlignment.Top,
-                ClipToBounds = true
+                ClipToBounds = true,
+                DataContext = this
             };
+            
+            flyoutControl.Loaded += OnLoaded;
 
-            Binding showFilterBinding = new Binding
-            {
-                Source = this,
-                Path = new PropertyPath("ShowFilter"),
-                Mode = BindingMode.TwoWay,
-                UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged
-            };
-            BindingOperations.SetBinding(flyoutControl, Flyout2.ShowFilterCheckedProperty, showFilterBinding);
-
-            Binding contentBinding = new Binding
-            {
-                Source = this,
-                Path = new PropertyPath("PlaceHolder1"),
-                Mode = BindingMode.TwoWay,
-                UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged
-            };
-            BindingOperations.SetBinding(flyoutControl, Flyout2.PlaceHolder1Property, contentBinding);
-
-            Binding clearFilterCommandBinding = new Binding
-            {
-                Source = this,
-                Path = new PropertyPath("ClearFilterCommand"),
-                Mode = BindingMode.TwoWay,
-                UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged
-            };
-            BindingOperations.SetBinding(flyoutControl, Flyout2.ClearFilterCommandProperty, clearFilterCommandBinding);
+            //Binding contentBinding = new Binding
+            //{
+            //    Source = this,
+            //    Path = new PropertyPath("PlaceHolder1"),
+            //    Mode = BindingMode.TwoWay,
+            //    UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged
+            //};
+            //BindingOperations.SetBinding(flyoutControl, Flyout2.PlaceHolder1Property, contentBinding);
 
             return flyoutControl;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            var flyout = sender as Flyout2;
+            if (flyout == null) return;
+            MyX = 25 - (int)flyout.ActualWidth;
+            MyY = 25 - (int)flyout.ActualHeight;
+            PlaceHolder1.DataContext = DataContext;
         }
 
         private bool ControlIsAdorned(UIElement control)
         {
             var layer = AdornerLayer.GetAdornerLayer(control);
             
-            //New
             if (layer == null) return false;
 
             var adorners = layer.GetAdorners(control);
--- FlyoutDemo/Flyout2.xaml.cs
+++ FlyoutDemo/Flyout2.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Windows;
-using System.Windows.Input;
 using System.Windows.Media;
 
 namespace FlyoutDemo
@@ -13,7 +12,7 @@ namespace FlyoutDemo
         public Flyout2()
         {
             InitializeComponent();
-            (Content as FrameworkElement).DataContext = this;
+            //(Content as FrameworkElement).DataContext = this;
             _arrowButtonMouseOverStyle = FindResource("OuterBorderStyle") as Style;
             _arrowButtonOpaqueStyle = FindResource("OuterBorderOpaqueStyle") as Style;
             OuterBorder.Style = _arrowButtonMouseOverStyle;
@@ -29,29 +28,23 @@ namespace FlyoutDemo
 
             //var x = PlaceHolder1.Width;
 
-            Loaded += OnLoaded;
+            //Loaded += OnLoaded;
 
         }
 
-        private void OnLoaded(object sender, RoutedEventArgs e)
-        {
-            Console.WriteLine("width " + PlaceHolder1.Width);
-            Console.WriteLine("Actual width " + PlaceHolder1.ActualWidth);
-            MyX = 25 - (int)ActualWidth;
-            MyY = 25 - (int)ActualHeight;
-        }
-
-        //public int MyX { get; set; } = -5;
-        //public int MyY { get; set; } = -10;
-        public int BorderHeight { get; set; } = 55;
-        public int BorderWidth { get; set; } = 125;
+        //private void OnLoaded(object sender, RoutedEventArgs e)
+        //{
+        //    MyX = 25 - (int)ActualWidth;
+        //    MyY = 25 - (int)ActualHeight;
+        //    //if (PlaceHolder1 != null)
+        //    //{
+        //    //    //PlaceHolder1.DataContext = RelativeSource
+        //    //}
+        //}
 
         #region DependencyProps
-        public static readonly DependencyProperty ClearFilterCommandProperty = DependencyProperty.Register(nameof(ClearFilterCommand), typeof(ICommand), typeof(Flyout2), new PropertyMetadata(null));
-        public static readonly DependencyProperty ShowFilterCheckedProperty = DependencyProperty.Register(nameof(ShowFilterChecked), typeof(bool), typeof(Flyout2), new PropertyMetadata(false));
         public static readonly DependencyProperty PlaceHolder1Property = DependencyProperty.Register(nameof(PlaceHolder1), typeof(FrameworkElement), typeof(Flyout2), new UIPropertyMetadata(null));
-        public static readonly DependencyProperty ContentXProperty = DependencyProperty.Register(nameof(ContentX), typeof(int), typeof(Flyout2), new PropertyMetadata(0));
-        public static readonly DependencyProperty ContentYProperty = DependencyProperty.Register(nameof(ContentY), typeof(int), typeof(Flyout2), new PropertyMetadata(0));
+        
         public static readonly DependencyProperty MyYProperty = DependencyProperty.Register(nameof(MyY), typeof(int), typeof(Flyout2), new PropertyMetadata(0));
         public static readonly DependencyProperty MyXProperty = DependencyProperty.Register(nameof(MyX), typeof(int), typeof(Flyout2), new PropertyMetadata(0));
 
@@ -66,35 +59,11 @@ namespace FlyoutDemo
             get { return (int)GetValue(MyXProperty); }
             set { SetValue(MyXProperty, value); }
         }
-
-        public int ContentY
+        
+        public FrameworkElement PlaceHolder1
         {
-            get { return (int)GetValue(ContentYProperty); }
-            set { SetValue(ContentYProperty, value); }
-        }
-
-        public int ContentX
-        {
-            get { return (int)GetValue(ContentXProperty); }
-            set { SetValue(ContentXProperty, value); }
-        }
-
-        public /*object*/FrameworkElement PlaceHolder1
-        {
-            get { return /*(object)*/(FrameworkElement) GetValue(PlaceHolder1Property); }
+            get { return (FrameworkElement) GetValue(PlaceHolder1Property); }
             set { SetValue(PlaceHolder1Property, value); }
-        }
-
-        public ICommand ClearFilterCommand
-        {
-            get { return (ICommand)GetValue(ClearFilterCommandProperty); }
-            set { SetValue(ClearFilterCommandProperty, value); }
-        }
-
-        public bool ShowFilterChecked
-        {
-            get { return (bool)GetValue(ShowFilterCheckedProperty); }
-            set { SetValue(ShowFilterCheckedProperty, value); }
         }
         #endregion
 
